27281e38c5ecdbfa3df96877a2018486d6ee5bf6
Elias Wennerlund <elias06wennerlund@gmail.com>
2024-02-28 22:59:28 +0100

    Basic parser types

--- PklParserCpp/Parser.cpp
+++ PklParserCpp/Parser.cpp
@@ -1,5 +1,39 @@
 #include "Parser.h"
 
+class Number
+{
+public:
+	explicit Number(numberVariants val) : value(val) {}
+private:
+	numberVariants value;
+};
+
+class Boolean
+{
+public:
+	explicit Boolean(bool val) : value(val) {}
+private:
+	bool value;
+};
+
+class String
+{
+public:
+	explicit String(const std::string& val) : value(val) {}
+private:
+	const std::string& value;
+};
+
+
+// Implement static member functions for PklValue
+Number PklValue::Number(numberVariants val) { return Number(val); }
+
+Boolean PklValue::Boolean(bool val) { return Boolean(val); }
+
+String PklValue::String(const std::string& val) { return String(val); }
+
+
+
 void Parser::parse(const std::string& input)
 {
 	Tokenizer tokenizer(input);
--- PklParserCpp/Parser.h
+++ PklParserCpp/Parser.h
@@ -1,6 +1,59 @@
 #pragma once
 #include <string>
+#include <variant>
 #include "Tokenizer.h"
+
+using valueTypes = std::variant<
+	std::variant<long, double>,
+	bool,
+	std::string
+>;
+
+using numberVariants = std::variant<long, double>;
+
+class Number;
+class Boolean;
+class String;
+
+// TODO:
+//class Duration;
+//class DataSize;
+//class Object;
+//class Listing;
+//class Mapping;
+//class Class;
+//class Method;
+// etc...
+
+class PklValue
+{
+
+public:
+	enum class PklType {
+		Number,
+		Boolean,
+		String,
+		//Duration,
+		//DataSize,
+		//Object,
+		//Listing,
+		//Mapping,
+		//Class,
+		//Method,
+		/// etc...
+		Null,
+	};
+
+	static Number Number(numberVariants val);
+	static Boolean Boolean(bool val);
+	static String String(const std::string& val);
+
+private:
+	PklType type;
+	valueTypes value;
+	
+	PklValue(PklType type, valueTypes value) : type(type), value(value) {}
+};
 
 class Parser
 {
